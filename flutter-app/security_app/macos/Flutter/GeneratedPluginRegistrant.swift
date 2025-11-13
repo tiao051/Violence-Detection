fe8bf2888967a8f0f5faa6eca7f9112c7d9f89c5
DoCaoThang <thangdo135789@gmail.com>
2025-11-14 04:49:52 +0700

    fix(auth): Clear provider cache on logout again

--- flutter-app/security_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter-app/security_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import firebase_core
 import firebase_messaging
+import google_sign_in_ios
 import shared_preferences_foundation
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
