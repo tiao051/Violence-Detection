fbf8ce8170ab12ce89cbff4090b64253cf26ba0b
tiao051 <nguyenminhtho0503@gmail.com>
2025-12-11 01:58:35 +0700

    refactor: remove obsolete configuration modules for model settings

--- ai_service/remonet/config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Configuration module.
-"""
-from .model_config import STEBackbone, DEFAULT_STE_BACKBONE, STE_BACKBONE_CONFIG
-
-__all__ = ['STEBackbone', 'DEFAULT_STE_BACKBONE', 'STE_BACKBONE_CONFIG']
