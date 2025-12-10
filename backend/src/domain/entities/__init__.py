23c963d83bf073d3a34d9f5293feb7c4aa07f73e
tiao051 <nguyenminhtho0503@gmail.com>
2025-12-11 02:02:10 +0700

    refactor: remove obsolete use cases, entities, and repository interfaces

--- backend/src/domain/entities/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Domain Entities - Enterprise Business Rules"""
-from .camera import Camera
-from .stream_session import StreamSession
-
-__all__ = ["Camera", "StreamSession"]
--- backend/src/domain/repositories/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Repository Interfaces - Abstract data access contracts"""
-from .camera_repository import ICameraRepository
-from .stream_repository import IStreamRepository
-
-__all__ = ["ICameraRepository", "IStreamRepository"]
