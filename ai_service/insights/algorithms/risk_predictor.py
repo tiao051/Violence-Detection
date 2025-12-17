b87eecee45b08173f57e789109e9159090285765
tiao051 <nguyenminhtho0503@gmail.com>
2025-12-17 17:38:48 +0700

    feat: remove unused pandas import from risk_predictor.py for cleaner code

--- ai_service/insights/algorithms/risk_predictor.py
+++ ai_service/insights/algorithms/risk_predictor.py
@@ -7,7 +7,6 @@ Predicts severity level given temporal/spatial conditions.
 from typing import List, Dict, Any, Optional, Tuple
 from collections import Counter
 import numpy as np
-import pandas as pd
 
 from . import BaseAnalyzer
 from ..time_utils import DAYS, PERIODS, categorize_hour
